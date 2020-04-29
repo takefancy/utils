79472907216d6c66a32fb098e16c855c32bf30ec
trangsinh87 <trangsinh87@yahoo.com.vn>
2020-04-30 00:19:28 +0700

    v1.1

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-'use strict';
-const glob = require('glob'),
-	generate = require('nanoid');
+"use strict";
+const glob = require("glob"),
+	generate = require("nanoid"),
+	phone = require("phone");
 
 let isArray = (e) => {
 		return e && e.constructor === Array;
 	},
 	isString = (e) => {
-		return typeof e === 'string';
+		return typeof e === "string";
 	},
 	union = (x, y) => {
 		var obj = {};
-		for (var i = x.length - 1; i >= 0; --i)
-			obj[x[i]] = x[i];
-		for (var i = y.length - 1; i >= 0; --i)
-			obj[y[i]] = y[i];
-		var res = []
+		for (var i = x.length - 1; i >= 0; --i) obj[x[i]] = x[i];
+		for (var i = y.length - 1; i >= 0; --i) obj[y[i]] = y[i];
+		var res = [];
 		for (var k in obj) {
-			if (obj.hasOwnProperty(k)) // <-- optional
+			if (obj.hasOwnProperty(k))
+				// <-- optional
 				res.push(obj[k]);
 		}
 		return res;
-	}
+	};
 
 function match(patterns, excludes) {
-	var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
+	var urlRegex = new RegExp("^(?:[a-z]+:)?//", "i");
 	var output = [];
 	if (isArray(patterns)) {
 		patterns.forEach((e) => {
@@ -38,10 +38,10 @@ function match(patterns, excludes) {
 				files = files.map((file) => {
 					if (isArray(excludes)) {
 						for (var i in excludes) {
-							file = file.replace(excludes[i], '');
+							file = file.replace(excludes[i], "");
 						}
 					} else {
-						file = file.replace(excludes, '');
+						file = file.replace(excludes, "");
 					}
 					return file;
 				});
@@ -53,46 +53,54 @@ function match(patterns, excludes) {
 }
 
 let phone = {
-	mask: function(e) {
+	mask: function (e) {
 		if (e) {
 			let ending = e.slice(e.length - 4);
 			return `(***) *** ${ending}`;
 		}
-		return '';
-	}
+		return "";
+	},
+	toE164: function (number, countryCode) {
+		return phone(number, countryCode || "USA")[0];
+	},
 };
 let random = {
 	string: (length) => {
-		return generate.customAlphabet('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', length || 6)();
-	}
+		return generate.customAlphabet(
+			"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			length || 6
+		)();
+	},
 };
 
 let color = {
 	contrast: (hexcolor) => {
 		// If a leading # is provided, remove it
-		if (hexcolor.slice(0, 1) === '#') {
+		if (hexcolor.slice(0, 1) === "#") {
 			hexcolor = hexcolor.slice(1);
 		}
 		// If a three-character hexcode, make six-character
 		if (hexcolor.length === 3) {
-			hexcolor = hexcolor.split('').map(function(hex) {
-				return hex + hex;
-			}).join('');
+			hexcolor = hexcolor
+				.split("")
+				.map(function (hex) {
+					return hex + hex;
+				})
+				.join("");
 		}
 		// Convert to RGB value
 		var r = parseInt(hexcolor.substr(0, 2), 16);
 		var g = parseInt(hexcolor.substr(2, 2), 16);
 		var b = parseInt(hexcolor.substr(4, 2), 16);
 		// Get YIQ ratio
-		var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+		var yiq = (r * 299 + g * 587 + b * 114) / 1000;
 		// Check contrast
-		return (yiq >= 128) ? 'black' : 'white';
-
-	}
+		return yiq >= 128 ? "black" : "white";
+	},
 };
 module.exports = {
 	match: match,
 	phone: phone,
 	random: random,
-	color: color
+	color: color,
 };
